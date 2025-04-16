74fdc5e72eb9d5e349e8f062db4b42846c50b3ea
Arild Matsson <arild.matsson@gu.se>
2025-04-16 12:30:21 +0200

    Instance plugin

--- modes/kubhist.ts
+++ modes/kubhist.ts
@@ -1,8 +1,7 @@
 import type { App } from 'vue'
-import type { ModePluginOptions } from '@/types/app.types'
 import settings from '@instance/settings'
 
-export async function install(app: App, options: ModePluginOptions) {
-  console.log(`Kubhist, Vue ${app.version}`, Object.fromEntries(options.params.entries()))
+export default async function install(app: App) {
+  console.log(`Kubhist, Vue ${app.version}`)
   settings.korp_backend_url += '/kubhist'
 }
