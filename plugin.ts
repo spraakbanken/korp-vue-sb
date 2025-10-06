666f75255e45a7416dc7a9e17488bf303bc14723
Arild Matsson <arild.matsson@gu.se>
2025-10-06 16:45:52 +0200

    Realer types, auth

--- plugin.ts
+++ plugin.ts
@@ -1,16 +1,17 @@
 import type { Plugin } from 'vue'
-import type { InstancePluginOptions } from '@/types/app.types'
 import searchComponents from '@instance/components/search'
 import settings from './settings'
+import authBasic from '@/auth/basic'
 
 // An async function returning a Vue 3 plugin: https://vuejs.org/guide/reusability/plugins
 // It is wrapped in an async function so that we can await dynamically imported code if needed.
-export default async function createPlugin(options: InstancePluginOptions): Promise<Plugin> {
+export default async function createPlugin(options: { mode: string }): Promise<Plugin> {
   // Default installer
   const install: Plugin = (app) => {
     // This is how we will provide named components and functions that can be referenced from config
     app.provide('korp.components.search', searchComponents) // attribute extended_component
     app.provide('korp.components.stringify', '...')
+    app.provide('korp.auth', authBasic)
   }
 
   // We can override the installer depending on the mode
--- settings.ts
+++ settings.ts
@@ -1,4 +1,4 @@
-import type { AppSettings } from '@/types/config.types'
+import type { InstanceConfig } from '@/core/config/instanceConfig.types'
 import settings from '@instance/config.yml'
 
-export default settings as AppSettings
+export default settings as InstanceConfig
