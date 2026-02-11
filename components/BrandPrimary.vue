14e2d41f0bb5cac311a243ecb75c9729a3595b89
Arild Matsson <arild.matsson@gu.se>
2026-02-11 16:37:50 +0100

    Logo with swe/eng and light/dark

--- components/BrandPrimary.vue
+++ components/BrandPrimary.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { useI18n } from "vue-i18n"
+import ColorSchemeImg from "@/components/ColorSchemeImg.vue"
 import korpSloganSwe from "@instance/assets/korp_slogan.svg"
+import korpSloganSweLight from "@instance/assets/korp_slogan_light.svg"
+import korpSloganEng from "@instance/assets/korp_slogan_en.svg"
+import korpSloganEngLight from "@instance/assets/korp_slogan_en_light.svg"
+
+const { locale } = useI18n()
 </script>
 
 <template>
-  <img :src="korpSloganSwe" alt="Korp logo" class="align-self-end" />
+  <ColorSchemeImg
+    :dark-src="locale == 'swe' ? korpSloganSweLight : korpSloganEngLight"
+    :light-src="locale == 'swe' ? korpSloganSwe : korpSloganEng"
+    alt="Korp slogan"
+    class="align-self-end"
+  />
 </template>
