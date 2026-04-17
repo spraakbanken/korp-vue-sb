84b2b7d081e52bd4d73f529dd3e4e7925717d58d
Arild Matsson <arild.matsson@gu.se>
2026-04-17 09:05:00 +0200

    Rephrase to "work in progress"

--- components/BrandSecondary.vue
+++ components/BrandSecondary.vue
@@ -5,7 +5,7 @@
     <div class="alert alert-danger d-inline-block m-0 p-1 px-2">
       <h6 class="m-0">
         <fa-icon icon="fa-solid fa-warning" />
-        Beta version
+        Work in progress
       </h6>
       <div><a href="https://github.com/spraakbanken/korp-vue-sb">About this version</a></div>
       <div><a href="https://spraakbanken.gu.se/korp/">Use normal Korp</a></div>
