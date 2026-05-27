bbaba0082a8b05bb8b1d5592cbe947ede0d2b852
Arild Matsson <arild.matsson@gu.se>
2026-05-27 08:38:06 +0200

    Fix settings import

--- modes/kubhist.ts
+++ modes/kubhist.ts
@@ -1,7 +1,7 @@
-import type { App } from 'vue'
-import settings from '@instance/settings'
+import type { App } from "vue"
+import settings from "@/core/config"
 
 export default async function install(app: App) {
   console.log(`Kubhist, Vue ${app.version}`)
-  settings.korp_backend_url += '/kubhist'
+  settings.korp_backend_url += "/kubhist"
 }
--- settings.ts
+++ settings.ts
@@ -1,4 +1,4 @@
-import type { InstanceConfig } from '@/core/config/instanceConfig.types'
-import settings from '@instance/config.yml'
+import type { InstanceConfig } from "@/core/config/instanceConfig.types"
+import settings from "@instance/config.yml"
 
 export default settings as InstanceConfig
