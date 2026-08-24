17d2fa2243ef438c7af21a54b97755c62ca4751b
Arild Matsson <arild.matsson@gu.se>
2026-08-24 10:27:39 +0200

    Add stringifiers for lemgram, sense, lemma

--- components/stringifiers.ts
+++ components/stringifiers.ts
@@ -1,16 +1,32 @@
 import { Lemgram } from "@/core/lemgram"
+import { Saldo } from "@/core/saldo"
+import { escape } from "lodash-es"
+
+/** Split by "+", transform and join back with " + " */
+const decompound = (item: string, parse?: (item: string) => string) =>
+  item
+    .split("+")
+    .map((part) => (parse ? parse(part) : part))
+    .join(" + ")
+
+const stripRank = (item: string) => item.replace(/:.*/, "")
 
 export function getStringifiers(t: (key: string) => string) {
   /** Format compound lemgrams separated by "+" */
-  const complemgram = (item: string) =>
-    item
-      .replace(/:.*$/, "")
-      .split("+")
-      .map((part) => Lemgram.parse(part)?.toHtml(t) || "(?)")
-      .join(" + ")
+  const complemgram = (item: string) => decompound(stripRank(item), lemgram)
 
+  /** Format compound word forms separated by "+" */
   // TODO Add `stringify: compwf` to compwf attribute config
-  const compwf = (item: string) => item.split("+").join(" + ")
+  const compwf = decompound
 
-  return { complemgram, compwf }
+  /** Format a lemgram */
+  const lemgram = (item: string) => Lemgram.parse(item)?.toHtml(t) || item
+
+  /** Format a word baseform */
+  const lemma = (item: string) => escape(item).replace(/_/g, " ")
+
+  /** Format a ranked SALDO sense */
+  const sense = (item: string) => Saldo.parse(stripRank(item))?.toHtml() || item
+
+  return { complemgram, compwf, lemgram, lemma, sense }
 }
