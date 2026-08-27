6bbe1a6443472f63cad5758662c9184cd1734063
Arild Matsson <arild.matsson@gu.se>
2026-08-27 11:28:30 +0200

    Use splitSuffix

--- components/stringifiers.ts
+++ components/stringifiers.ts
@@ -1,5 +1,6 @@
 import { Lemgram } from "@/core/lemgram"
 import { Saldo } from "@/core/saldo"
+import { splitSuffix } from "@/core/util"
 import { escape } from "lodash-es"
 
 /** Split by "+", transform and join back with " + " */
@@ -9,24 +10,21 @@ const decompound = (item: string, parse?: (item: string) => string) =>
     .map((part) => (parse ? parse(part) : part))
     .join(" + ")
 
-const stripRank = (item: string) => item.replace(/:.*/, "")
-
 export function getStringifiers(t: (key: string) => string) {
   /** Format compound lemgrams separated by "+" */
-  const complemgram = (item: string) => decompound(stripRank(item), lemgram)
+  const complemgram = (item: string) => decompound(splitSuffix(item)[0], lemgram)
 
   /** Format compound word forms separated by "+" */
-  // TODO Add `stringify: compwf` to compwf attribute config
   const compwf = decompound
 
   /** Format a lemgram */
   const lemgram = (item: string) => Lemgram.parse(item)?.toHtml(t) || item
 
   /** Format a word baseform */
-  const lemma = (item: string) => escape(item).replace(/_/g, " ")
+  const lemma = (item: string) => splitSuffix(escape(item))[0].replace(/_/g, " ")
 
   /** Format a ranked SALDO sense */
-  const sense = (item: string) => Saldo.parse(stripRank(item))?.toHtml() || item
+  const sense = (item: string) => Saldo.parse(splitSuffix(item)[0])?.toHtml() || item
 
   return { complemgram, compwf, lemgram, lemma, sense }
 }
