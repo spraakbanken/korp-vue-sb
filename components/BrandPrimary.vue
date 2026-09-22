54e4dd6f9585f9b55094a0958d0692532121ec20
Arild Matsson <arild.matsson@gu.se>
2026-09-22 16:13:11 +0200

    Lab indicator with popover

--- components/BrandPrimary.vue
+++ components/BrandPrimary.vue
@@ -1,23 +1,54 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n"
+import { vPopover } from "@/bootstrap"
 import ColorSchemeImg from "@/components/ColorSchemeImg.vue"
 import korpSloganSwe from "@instance/assets/korp_slogan.svg"
 import korpSloganSweLight from "@instance/assets/korp_slogan_light.svg"
 import korpSloganEng from "@instance/assets/korp_slogan_en.svg"
 import korpSloganEngLight from "@instance/assets/korp_slogan_en_light.svg"
+import { computed } from "vue"
 
-const { locale } = useI18n()
+const { locale, t } = useI18n()
 
+/** Link to homepage, i.e. without any hash params */
 const selfUrl = window.location.href.replace(/#.*/, "")
+const isLab = import.meta.env.VITE_LAB
+
+// TODO Improve keyboard navigation; this link appears last in tab order
+const labTooltip = computed(
+  () =>
+    `<div><strong>${t("lab.heading")}</strong> ${t("lab.help")}</div>
+    <div><a href="/korp#${window.location.hash}">${t("lab.link")}</a></div>`,
+)
 </script>
 
 <template>
-  <a :href="selfUrl">
-    <ColorSchemeImg
-      :dark-src="locale == 'swe' ? korpSloganSweLight : korpSloganEngLight"
-      :light-src="locale == 'swe' ? korpSloganSwe : korpSloganEng"
-      alt="Korp slogan"
-      class="align-self-end"
-    />
-  </a>
+  <div>
+    <div class="d-inline-block position-relative">
+      <a :href="selfUrl">
+        <ColorSchemeImg
+          :dark-src="locale == 'swe' ? korpSloganSweLight : korpSloganEngLight"
+          :light-src="locale == 'swe' ? korpSloganSwe : korpSloganEng"
+          alt="Korp"
+          class="align-self-end"
+        />
+      </a>
+
+      <!-- Lab mode indicator -->
+      <button
+        v-if="isLab"
+        type="button"
+        v-popover
+        data-bs-toggle="popover"
+        data-bs-html="true"
+        :data-bs-content="labTooltip"
+        :key="locale"
+        style="cursor: help"
+        class="position-absolute top-0 end-0 border-0 badge rounded-pill bg-warning fs-6"
+      >
+        <fa-icon icon="flask" />
+        {{ $t("lab") }}
+      </button>
+    </div>
+  </div>
 </template>
--- fontawesome.ts
+++ fontawesome.ts
@@ -1,6 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faCopy, faPlay, faRotateLeft, faWarning } from "@fortawesome/free-solid-svg-icons"
+import { faCopy, faFlask, faPlay, faRotateLeft, faWarning } from "@fortawesome/free-solid-svg-icons"
 import {} from "@fortawesome/free-regular-svg-icons"
 
 // Search icons at https://fontawesome.com/search?ic=free-collection
-library.add(faCopy, faPlay, faRotateLeft, faWarning)
+library.add(faCopy, faFlask, faPlay, faRotateLeft, faWarning)
